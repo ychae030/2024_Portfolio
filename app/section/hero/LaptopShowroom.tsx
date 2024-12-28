726e0ee284b1ab46dfb609261079a1939da52021
ychae030 <ychae1997@gmail.com>
2024-12-29 02:25:48 +0900

    Fix: 빌드 시 서버에서 GLTF 로드 문제 해결

--- app/section/hero/LaptopShowroom.tsx
+++ app/section/hero/LaptopShowroom.tsx
@@ -1,19 +1,32 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Laptop from "./Laptop";
 import { Canvas } from "@react-three/fiber";
 import ScrollCamera from "./ScrollCamera";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { useLoader } from "@react-three/fiber";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import Loading from "../../components/Loading";
 import { useLoading } from "@/hooks/useLoading";
 
 export default function LaptopShowroom() {
-  const glb = useLoader(GLTFLoader, "/models/laptop.glb"); // GLTF 파일 로드
+  const [glb, setGlb] = useState<GLTF | null>(null); // GLTF 상태 관리
   const isLoading = useLoading(glb); // 로딩 상태 관리
 
-  if (isLoading) {
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.load(
+      "/models/laptop.glb",
+      (data: GLTF) => {
+        setGlb(data); // GLTF 데이터 설정
+      },
+      undefined,
+      (error) => {
+        console.error("Error loading GLTF:", error);
+      }
+    );
+  }, []);
+
+  if (isLoading || !glb) {
     return <Loading />;
   }
 
